cdc861ea81bffdc79a487a0d8769d086272f7f2d
Harshil Shah <harshilshah1910@outlook.com>
2018-12-02 18:42:17 +0530

    Reimplemented the whole thing as a Playground

---
+++ AdventOfCode.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,310 @@
+//: [Previous](@previous)
+import Foundation
+
+let input = """
+uqcipadzntnheslgvjjozmkfyr
+uqcipadzwtnhexlzvxjobmkfkr
+cqcipadpwtnheslgyxjobmkfyr
+ubnipadzwtnheslgvxjobmkfyw
+uqcisadzwtnheslgvxjfbmkfor
+uqcisaezwtnheslgvxkobmkfyr
+uqcguadzwtnheslgvxjobmkfir
+uqcipadzmtnhesldvxdobmkfyr
+uqcipadzwtzheslgdxjtbmkfyr
+uquipadzwtcheslgvxjobmkfbr
+uqctpadzwtnhesjbvxjobmkfyr
+ueciparzwtnheslgvxjobmkfyx
+uqcipadzwtnhessgvxjkbmkfkr
+uqcipamzwtnheslgvxioamkfyr
+uciizadzwtnheslgvxjobmkfyr
+uqcieadzwtnhesfgvxeobmkfyr
+fqcipadzwtnreslgvkjobmkfyr
+uqcipadzrtnherlgvxjobmklyr
+uqcypadzwtnheslgvxjobmkxfr
+uqcipadzwtnhemlgvxjobmvfur
+uwciuadzwwnheslgvxjobmkfyr
+uqcipadzwtnhcscgvxjobmkuyr
+upripadzwtnheslovxjobmkfyr
+uqcipadzltnheslgvxjobmkftc
+uqcipadzwtnheslgvgjobmifsr
+uqoipadzwtnheslgvxjosmkfkr
+uqcipadzwtbhesrqvxjobmkfyr
+uqcipadzwtnheslpvxjobmhfyx
+uhcinadzwtnheslgvxjybmkfyr
+uqcipadzwtnhhslgvxjabmkbyr
+uecipadzwtnheslgvxjobqyfyr
+uqcipadfwtnheslwvxjobgkfyr
+uqcipadzvtnheshgvxzobmkfyr
+fqcipadzwtcheslgvxjobmkfyt
+uecipadzwtnheslgpxjbbmkfyr
+uqclpadzwtnheslgvnjobukfyr
+qqciprdzetnheslgvxjobmkfyr
+uqcipahpwtnheslgvxjtbmkfyr
+uqcidadzwtnhesljvxyobmkfyr
+uqciradswtnqeslgvxjobmkfyr
+uqcipadzwtrhmslgvxjobmkfyf
+urcipadzjtnheslgvxfobmkfyr
+uqcipadzwznheslgvxjobmkfcv
+uqcipadowtnheslgyxjobmkfym
+uqcigadzwtnheslgvxjoomkmyr
+uqjipafzwtnheslgvejobmkfyr
+uqcioadzwtnhhslgvxzobmkfyr
+uqcgpadkwtnheslgvxjobhkfyr
+ufciiadewtnheslgvxjobmkfyr
+uqoipadzwtnheslgvxjllmkfyr
+uqcipadzutnheslgwxxobmkfyr
+uqcipadzwtlheslgaxjobmkfwr
+uqcbpadzutnheslgvxjbbmkfyr
+uucipadzwvnhesngvxjobmkfyr
+uqcifadzwtnceslgvxjoumkfyr
+ujcipadzwteheslgvxjobmkfyj
+uqcipadzwtnheslqvxjobmkuyp
+uqcipadzwtnheslgvxjoxmkxyw
+uqcipaduwtnheslgvujmbmkfyr
+uicipadnwtnheslgvxjobmbfyr
+uqcipadzwteheslgvxjobbmfyr
+uqcipadzwgnneslgvxjobmklyr
+uqcipadzxtnhwslgvbjobmkfyr
+uqcipaxwwtnheslxvxjobmkfyr
+uocipadzwtnheslgvxjobqdfyr
+uqciaauzwtnheslgtxjobmkfyr
+uncipagzwtnkeslgvxjobmkfyr
+uqcipadzwtnhehlgvxjohdkfyr
+uqcipadzwtnheslgvxjobmspyz
+uccipadzwtnhvsltvxjobmkfyr
+uacipagzwtnheslgvxjoqmkfyr
+tqcipaduwtnheslgvxjobmmfyr
+uqcipadzwtnheslgvxqebmifyr
+uecipadthtnheslgvxjobmkfyr
+uocipadzwtnhdslgvkjobmkfyr
+uqcipadtwtnheslgvxhobmufyr
+uqkipadzwtnleslgtxjobmkfyr
+uqcipadzjunheslgvxjobmnfyr
+ubcipadzwtvheslgvxjobmkfyf
+uqcipadzwpfheslgvxjsbmkfyr
+uocipadzwtndeslgvxjobmmfyr
+uqcipadzwtnheslgtxjobhkfyq
+uqcipadzwtrheslgvxjobmyfya
+uqcipadzwtvheslgvxjolgkfyr
+uqcipidzwtaheslgvxjobmkfxr
+uyzixadzwtnheslgvxjobmkfyr
+uqyihadzwtnhedlgvxjobmkfyr
+uqcipadzwtnhesltvejobqkfyr
+uqciptdzwtnheslgyxlobmkfyr
+uqcipzdzwtnhzslgvxjosmkfyr
+uqcipadzwtnbeslgexjobmkfvr
+uqcipadzwtnheslcwxjobmkkyr
+uqcapadzwcnheslgvxjolmkfyr
+uqcjpadzwtnhejlgvxjxbmkfyr
+uqcipadwwtxweslgvxjobmkfyr
+uqmipadzwtnhezlgvxjobmkyyr
+uqcipubzwtnpeslgvxjobmkfyr
+uecvpadzwtnheslgvxjocmkfyr
+uqcipadzwfnheslgvxjibmkdyr
+uqcipadzwtnheslgvxvfbykfyr
+uqcipadzwtnheslgvgjoimkfyt
+dqcqpaqzwtnheslgvxjobmkfyr
+uqcipbdzwtnheslgvxjobmkghr
+jqcipadzwtnheslgvxjgbmkzyr
+uqcipadzwtnheslgvxqkqmkfyr
+uqcipadzptnheslgvxjxbokfyr
+uucijadzwtwheslgvxjobmkfyr
+uccfpadzwtnheslgvxjobpkfyr
+uqcipadzwtnheslgvxjobakeyq
+uqcipadzwtnheolgvxqobjkfyr
+imiipadzwtnheslgvxjobmkfyr
+uqcehadzwtnheslgvxjobmkuyr
+uqcipadzztnheslgvxjorokfyr
+rqcixadzwtnheelgvxjobmkfyr
+uqcipadzwtzheslgvxjodmkfyi
+uqcipaezwtnwuslgvxjobmkfyr
+uqcipadzwtnheslggxjobjkfyq
+uqcipadzwkghesagvxjobmkfyr
+uqcypqdzwtnheslgvxjobakfyr
+iqcipadzwtnhezltvxjobmkfyr
+uxcimadzwtnheslgvxjobmxfyr
+uqcipaizwtvhwslgvxjobmkfyr
+uqcipafzwtnheslgvxjpbmkfym
+uqcipadzwinheslgvxlobmpfyr
+uqcupadzwtnheslkvxmobmkfyr
+uqcapadzwtnhesrgvxjobmkfsr
+urcipafzwtnheslgvxjobmkfur
+uqcipaczwtnheslgvbjobmknyr
+uqcizadzztgheslgvxjobmkfyr
+uqcipfdzwtnhesxgvxjobmkfyw
+uqcipbdzwtnhyslgvxjobmcfyr
+uqcipadzwanhezlgvxjobmkfwr
+uvcipadzwtnheslgvxjbkmkfyr
+uqcipajzwtnseslgvxjobmkfyq
+uqcipvdzwtnheslgvmlobmkfyr
+uqcipadzdgnheslgmxjobmkfyr
+uqcipddzwtnhestgvpjobmkfyr
+umcipadzwtdheslgvxjzbmkfyr
+uqciuwdzwtnheslgvxjobmkflr
+uqcipadzwtnheslgsxabbmkfyr
+uceipadzwtnheslgvxjobgkfyr
+mqcipadzwtnhesrgvxjobmjfyr
+aqcipadvwtnheslgvxjobmkryr
+uqsipadzwtnofslgvxjobmkfyr
+uqcixadzwtfheslgvxjzbmkfyr
+uqcipadnwfnheslgvxjohmkfyr
+uqcivadzwtnheslfvxjobmkfyz
+uqciprdzwtnheslgvxjobmkjir
+uqcipadhbtnheslgvxjoxmkfyr
+fqcipadzwtnhesfgvxjobmkfye
+uqoipqdzwtnheqlgvxjobmkfyr
+uqcipadzwtnhesltvxmobmkzyr
+uqcipadzwtnhebqgvsjobmkfyr
+uqcipadzwtnheslglxjobmfbyr
+gqcipadzwtgheslgvxjobwkfyr
+uqcipadzwtnheslgfxjzbmlfyr
+ujcnpadzwtnheslrvxjobmkfyr
+ujcivadzwtnheglgvxjobmkfyr
+uqcitadzwgnheslgvxjofmkfyr
+uqcipahzatnhmslgvxjobmkfyr
+uqzipaizwtnheslgvujobmkfyr
+uqcipadzltnheylgvnjobmkfyr
+uqcidadzwtnhwsljvxyobmkfyr
+uqcipadzwtihetlgvxjobhkfyr
+oqcipabzwtnheslgvfjobmkfyr
+uqcipadzwtnveslgvxjobzkfzr
+uqcipadzwtjheslgqxjobmlfyr
+uqcnpadzztnheslgvxjobmkoyr
+uqciuadzwonheslgvxjobmkfyz
+tqcipadzwtnheslgvxaobmqfyr
+uqcipadtwtnhqslgvxjobmkeyr
+uqcipadzwbnheslgvajobmsfyr
+ubcopadzwtnhgslgvxjobmkfyr
+uqcipydzwtwheslgvxjobakfyr
+cqbijadzwtnheslgvxjobmkfyr
+uscipadowtnheslgvxjobmkfcr
+uqcipadzwtgheslnvxjobskfyr
+uqcipzdzwtnzeslgkxjobmkfyr
+uqcipawzwtnhrslgbxjobmkfyr
+uqcipadzatchyslgvxjobmkfyr
+uqcipadzotnpeslgvxjobmjfyr
+uqcipagzwtnheslgvxjobmvfyt
+uqcipadzwhnheslgvxyobmkfyo
+uqcipadzwtnheslgmqjobmkfyc
+uqcupadzwgnheslgvcjobmkfyr
+uqcipabzwbnheslgvxjobmkwyr
+uqciiadzwtnheslgvxjobmkfmz
+uqkipauzwtnheslgvxjjbmkfyr
+uqcipidzetnheslgvxjobmkfyi
+uqcipadzwtnheslgqxjokmkfmr
+uqcipadzqtnhesllvxjobmkfyk
+uqccpadzwtnheslgmxsobmkfyr
+uqcipadzwteheslgvljfbmkfyr
+uqcipadxwinheslgaxjobmkfyr
+uqcipadzwtnheslhvxyobmkfjr
+aqcipadzwnnheslgvxjqbmkfyr
+uvcipadzwtnheszgvxjobmkfyg
+uqcipahzmtnheslgvxjobmkfir
+ukcipadzbtnheslgvxjobmkfyb
+uqcipadzwtnhemlgvqjobmkfpr
+uqcipadzwtnheslgvmeobmkfpr
+uqciphdrwtnheslgvxjobmkfyw
+uqcipadzwtnheslevxqobzkfyr
+uqcipadzwknzeslgvxnobmkfyr
+wqcipadzwjnheslgvxjobbkfyr
+uqcipadzwtdheslgvmjobmkjyr
+uqvipadzwtnhextgvxjobmkfyr
+uqhipadzwtnheslwvxjzbmkfyr
+uqcipadzwtnherlgsxjobmksyr
+uqcipadzwtnhesqgvxjotmvfyr
+udcipadzwtnhekwgvxjobmkfyr
+uqcjprdzwtnheslgvxjobmkfpr
+uqcipadzatnheclgvqjobmkfyr
+uqcbpadzctnheslqvxjobmkfyr
+uqcipadzqtnhesluvxjobrkfyr
+uqcipadzwtnhcslgvxjoomwfyr
+uqcppadzwxnheslgwxjobmkfyr
+uqcipadcwtnheslrvxjdbmkfyr
+ukcipadzwtnhhslgvxjobmkgyr
+uqckpadzwtnheslgvxjokmkiyr
+uqcspadzwtjheslgvxjobmkfjr
+uqcipadpwtnhsslgvxjobmkfyu
+uqcepadzwtnheilgvbjobmkfyr
+jqcipadiwtnheslgvxjobmkjyr
+uqcipadzrtnseslgqxjobmkfyr
+sqmipadzwtnhewlgvxjobmkfyr
+uqcieadzhtnheslgvgjobmkfyr
+uqcipadzwkwhewlgvxjobmkfyr
+uqcipadzwtzheslgvxjpbqkfyr
+uzcipadzjtnheslgvxjobmlfyr
+uqcipadzwtnheslnvxjobmkfee
+uqciyanzwtnheslgvxjoimkfyr
+uqcipadqwtnheswghxjobmkfyr
+uycipadzwtnheslovxjobmofyr
+uqcipadzwtnheslgvxcozmxfyr
+uqmipadzwtnxezlgvxjobmkfyr
+uqcipadzftnheslgvxjotmkffr
+aqcipaizwtnhesagvxjobmkfyr
+uqcipcdzwtnheslgoajobmkfyr
+uqcypadgwtnhesbgvxjobmkfyr
+uqcipcdzwtnheslgvxjebmkfyb
+uhcvpadzwtnheslgvxjobzkfyr
+uqcipadzwtnpesagvxmobmkfyr
+uqcipadzwtnidslgvxjobmkfor
+uqcipadkwtnhesigvxjzbmkfyr
+uqcypadlwtnheslsvxjobmkfyr
+qqcipadzwtnheswgvxjobmkoyr
+uqcipadzwtnheslgvxjhbmmcyr
+uqcipadzwtnhesogvxjormkfmr
+uqcipadzwtnhetcgvxgobmkfyr
+"""
+
+func partOne() -> String {
+    let text = input
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+        .components(separatedBy: .newlines)
+    
+    var twoRepeats = 0
+    var threeRepeats = 0
+    
+    text.forEach { line in
+        var counts = [Character: Int]()
+        line.forEach { counts[$0, default: 0] += 1 }
+        
+        if counts.contains(where: { $0.value == 2 }) {
+            twoRepeats += 1
+        }
+        
+        if counts.contains(where: { $0.value == 3 }) {
+            threeRepeats += 1
+        }
+    }
+    
+    return "\(twoRepeats * threeRepeats)"
+}
+
+func partTwo() -> String {
+    let text = input
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+        .components(separatedBy: .newlines)
+    
+    for (indexOne, valueOne) in text.enumerated() {
+        for (_, valueTwo) in Array(text.dropFirst(indexOne + 1)).enumerated() {
+            var diff = 0
+            
+            for (c1, c2) in zip(valueOne, valueTwo) {
+                if c1 != c2 {
+                    diff += 1
+                }
+                
+                if diff > 1 {
+                    break
+                }
+            }
+            
+            if diff == 1 {
+                return String(zip(valueOne, valueTwo).compactMap { return ($0 == $1) ? $0 : nil })
+            }
+        }
+    }
+    
+    return "This didn't work"
+}
+
+print(partOne())
+print(partTwo())
--- Source/main.swift
+++
@@ -1,9 +0,0 @@
-//
-//  main.swift
-//  AdventOfCode
-//
-//  Created by Harshil Shah on 30/11/18.
-//  Copyright © 2018 Harshil Shah. All rights reserved.
-//
-
-import Foundation
